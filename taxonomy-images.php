6735199b3b85e2a1e076c9f4701ff5f5fbe7c72c
Ben Huson <ben@thewhiteroom.net>
2016-08-11 07:07:36 +0100

    Move `public-filters.php` to includes folder.

--- taxonomy-images.php
+++ taxonomy-images.php
@@ -33,7 +33,7 @@ Taxonomy_Images_Config::set_plugin_file( __FILE__ );
 
 require_once( trailingslashit( dirname( __FILE__ ) ) . 'includes/term.php' );
 require_once( trailingslashit( dirname( __FILE__ ) ) . 'deprecated.php' );
-require_once( trailingslashit( dirname( __FILE__ ) ) . 'public-filters.php' );
+require_once( trailingslashit( dirname( __FILE__ ) ) . 'includes/public-filters.php' );
 
 /**
  * Detail Image Size.
