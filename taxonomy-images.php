fa4de973c36b3afe1827af58f974524c1d2131c7
Ben Huson <ben@thewhiteroom.net>
2017-07-04 07:21:53 +0100

    Only load settings admin in the admin.

--- taxonomy-images.php
+++ taxonomy-images.php
@@ -37,7 +37,9 @@ require_once( trailingslashit( dirname( __FILE__ ) ) . 'includes/public-filters.
 require_once( trailingslashit( dirname( __FILE__ ) ) . 'deprecated.php' );
 
 // Admin Only
-require_once( trailingslashit( dirname( __FILE__ ) ) . 'includes/settings-admin.php' );
+if ( is_admin() ) {
+	require_once( trailingslashit( dirname( __FILE__ ) ) . 'includes/settings-admin.php' );
+}
 
 /**
  * Version Number.
